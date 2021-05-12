e0a8f443dcb3618103dcf9843b535d6da8ba3844
Kryztof55 <c.farango.dg@gmail.com>
2021-05-11 19:08:36 -0500

    Backend is working

---
+++ backend/models/record.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const recordSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    number: { type: String, required: true },
+    mail: { type: String, required: true },
+    birthday: { type: Date, required: false },
+    avatar: { type: String, required: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Record = mongoose.model("Record", recordSchema);
+
+module.exports = Record;
---
+++ backend/routes/records.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const router = require("express").Router();
+
+let Record = require("../models/record.model");
+
+router.route("/").get((req, res) => {
+  Record.find()
+    .then((record) => res.json(record))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res, next) => {
+  const name = req.body.name;
+  const number = Number(req.body.number);
+  const mail = req.body.mail;
+  const birthday = req.body.birthday;
+  const avatar = req.body.avatar;
+
+  const newRecord = new Record({
+    name,
+    number,
+    mail,
+    birthday,
+    avatar,
+  });
+
+  newRecord
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  Record.findById(req.params.id)
+    .then((record) => res.json(record))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Record.findByIdAndDelete(req.params.id)
+    .then(() => res.json("User deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Record.findById(req.params.id)
+    .then((record) => {
+      record.name = req.body.name;
+      record.number = Number(req.body.number);
+      record.mail = req.body.mail;
+      record.avatr = req.body.avatr;
+
+      record
+        .save()
+        .then(() => res.json("User updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
